4f3cdf66a8a9a66a65542e2fd4a47f6d2b708d3e
Jagroop <jagroopbagri5@gmail.com>
2022-04-13 23:45:48 -0700

    created function tthat will append users to the DOM

--- index.js
+++ index.js
@@ -8,3 +8,38 @@ const retrieveUsers = async () => {
   // return the user object
   return userObject;
 };
+
+// function that appends users to the DOM
+
+const appendUsers = (array) => {
+  // target the user container div element
+  let userContainer = document.querySelector('.users--container');
+  // loop over array and create DOM elements
+  array.forEach((user) => {
+    // create div that surrounds user name
+    let nameContainer = document.createElement('div');
+    // add class to name container
+    nameContainer.classList.add('name--container');
+    // insert user name into container
+    nameContainer.textContent = `${user.name}`;
+    // append contents to DOM
+    userContainer.appendChild(nameContainer);
+  });
+};
+
+// main function that runs the app
+const app = async () => {
+  // declare userObject Variable
+  let userObject;
+  // try to retreive API data
+  try {
+    //create user object using retrieve users object
+    userObject = await retrieveUsers();
+    // alert error if fetch is not successful
+  } catch (e) {
+    alert('Error!');
+  }
+  appendUsers(userObject);
+};
+
+app();
